af609f5f4ea1ed4044fd9745f4b9680cde980916
Arun Praba <arunpraba.k@gmail.com>
2020-05-08 08:01:55 +0530

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Root = () => {
+  return <div>Lyrical</div>
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'))
---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./server/server')
+
+app.listen(4000, () => {
+  console.log(`Listening http://localhost:4000/graphql`)
+})
